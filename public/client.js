841cf5496158474043b929cf1f329e3c3580b167
diptowebhero <diptodas1672@gmail.com>
2022-07-26 20:29:41 +0600

    done

---
+++ public/client.js
@@ -0,0 +1,186 @@
+const socket = io();
+
+//selection
+const nameFormsArea = document.querySelector(".nameForms");
+const nameForm = document.querySelector("#nameForm");
+const msgForm = document.querySelector("#msg_form");
+const roomArea = document.querySelector(".room");
+const onlineUserList = document.querySelector("#onlineUserList");
+const msgEmpty = document.querySelector(".msg-empty");
+const innerCanvas = document.querySelector(".inner_canvas");
+const displayName = document.querySelector(".displayName");
+const messages = document.querySelector(".messages");
+const createRoomInput = document.querySelector("#create_room");
+const createRoomBtn = document.querySelector("#create-btn");
+const publicRoomDiv = document.querySelector("#accordionPanelsStayOpenExample");
+const modal = document.querySelector(".modal");
+
+//GLOBALS VARIABLE
+let activeUsers;
+let publicRooms;
+
+//SET NAME EVENT
+nameForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = nameForm[0].value;
+  if (!name) return;
+  socket.emit("set_name", name, () => {
+    nameFormsArea.hidden = true;
+    roomArea.hidden = false;
+    nameForm[0].value = "";
+  });
+});
+
+//GET ACTIVE USERS
+socket.on("active_user", (users) => {
+  onlineUserList.innerHTML = "";
+  activeUsers = users;
+  users.forEach((user) => {
+    const li = document.createElement("li");
+    li.style.cursor = "pointer";
+    li.style.fontWeight = "600";
+    li.addEventListener("click", () => {
+      openCanvas(user);
+      msgForm[1].dataset.room = false;
+      messages.innerHTML = "";
+    });
+    li.classList.add("user_list");
+    li.classList.add("list-group-item");
+    li.classList.add("px-4");
+    li.classList.add("onLine");
+    li.textContent = user.id === socket.id ? "You" : user.name;
+    onlineUserList.appendChild(li);
+  });
+});
+
+//SEND PRIVATE MESSAGE
+msgForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = msgForm[0].value;
+  const id = msgForm[1].value;
+  const isRoom = msgForm[1].dataset.room;
+  //send message event
+  socket.emit("send_message", { message, id, isRoom }, () => {
+    if (!message) return;
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.classList.add("send_message");
+    li.style.background = "#78E378";
+    li.style.color = "#000";
+    li.style.marginLeft = "20%";
+    li.textContent = `You : ${message}`;
+    messages.appendChild(li);
+    msgForm[0].value = "";
+  });
+});
+
+//RECEIVE MESSAGE
+socket.on("receive_message", (data, senderId) => {
+  const isRoom = data.isRoom;
+  const users = activeUsers.find((user) => user.id === data.id);
+  // openCanvas(users);
+  const sender = activeUsers.find((user) => user.id === senderId);
+  if (isRoom) {
+    innerCanvas.hidden = false;
+    displayName.textContent = data.id;
+    msgForm[1].value = data.id;
+    msgForm[1].dataset.room = true;
+    msgEmpty.classList.add("d-none");
+  } else {
+    openCanvas(sender);
+    msgForm[1].dataset.room = true;
+  }
+  const li = document.createElement("li");
+  li.classList.add("list-group-item");
+  li.classList.add("send_message");
+  li.textContent = `${sender.name} : ${data.message}`;
+  messages.appendChild(li);
+});
+
+//OPEN CANVAS
+function openCanvas(user) {
+  msgEmpty.classList.add("d-none");
+  displayName.textContent = user.name;
+  innerCanvas.hidden = false;
+  msgForm[1].value = user.id;
+}
+
+//CREATE ROOMS
+createRoomBtn.addEventListener("click", () => {
+  const roomName = createRoomInput.value;
+  if (!roomName) return;
+  socket.emit("create_room", roomName, () => {
+    closeModal();
+  });
+});
+
+//GET PUBLIC ROOM
+socket.on("get_public_room", (rooms) => {
+  publicRooms = rooms;
+  publicRoomDiv.innerHTML = "";
+  rooms.forEach((room) => {
+    const accordionItem = document.createElement("div");
+    accordionItem.classList.add("accordion-item");
+    accordionItem.innerHTML = `
+    <div class="accordion-item">
+    <h2 class="accordion-header" id="${room.id}id">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${room.id}option" aria-expanded="false" aria-controls="${room.id}option">
+        ${room.roomName} (${room.size})
+        <span onclick="joinRoom('${room.roomName}')"  class="material-symbols-outlined mx-1">
+                      group_add
+                  </span>
+                  <span 
+                  onclick="leaveRoom('${room.roomName}')"
+                  class="material-symbols-outlined mx-1"> logout </span>
+      </button>
+    </h2>
+    <div id="${room.id}option" class="accordion-collapse collapse" aria-labelledby="${room.id}id" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <ul id="participants">
+        
+        </ul>
+      </div>
+    </div>
+  </div>
+    `;
+    const participantsUl = accordionItem.querySelector("#participants");
+    room?.participants?.forEach((participant) => {
+      const li = document.createElement("li");
+      li.classList.add("list-group-item");
+      li.classList.add("onLineRoom");
+      li.textContent = participant.name;
+      participantsUl.appendChild(li);
+    });
+    publicRoomDiv.appendChild(accordionItem);
+  });
+});
+
+//JOIN ROOM EVENT
+function joinRoom(roomName) {
+  socket.emit("join_room", roomName, () => {
+    messages.innerHTML = "";
+    console.log("User join");
+    innerCanvas.hidden = false;
+    msgEmpty.classList.add("d-none");
+    displayName.textContent = roomName;
+    msgForm[1].value = roomName;
+    msgForm[1].dataset.room = true;
+  });
+}
+
+//CLOSE MODAL
+function closeModal() {
+  modal.classList.remove("show");
+  modal.style.display = "none";
+  createRoomInput.value = "";
+  document.body.classList.remove("modal-open");
+  document.body.style = {};
+  document.querySelector(".modal-backdrop")?.remove("show");
+}
+
+//LEAVE ROOM
+function leaveRoom(roomName) {
+  socket.emit("leave_room", roomName, () => {
+    innerCanvas.hidden = true;
+  });
+}
---
+++ server.js
@@ -0,0 +1,138 @@
+//dependencies
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+//app initialize
+const app = express();
+const expressServer = http.createServer(app);
+
+app.use(express.static("public"));
+
+//application route
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+//socket server
+const io = new Server(expressServer);
+
+//connecting socket
+io.on("connection", (socket) => {
+  //get online user
+  async function getActiveUsers() {
+    const activeUsersSocket = io.sockets.sockets;
+    const sids = io.sockets.adapter.sids;
+    const activeUsersArray = [];
+    const activeUsersId = [...sids.keys()];
+    activeUsersId.forEach((userId) => {
+      const { name, id } = activeUsersSocket.get(userId);
+      if (name) {
+        activeUsersArray.push({
+          name,
+          id,
+        });
+      }
+    });
+    return activeUsersArray;
+  }
+
+  //get public rooms
+  async function getPublicRooms() {
+    const rooms = await io.sockets.adapter.rooms;
+    const sids = await io.sockets.adapter.sids;
+    const allSocket = await io.sockets.sockets;
+
+    const roomsKeys = [...rooms.keys()];
+    const sidsKeys = [...sids.keys()];
+    const publicRooms = [];
+    let roomId = 0;
+    for (let roomName of roomsKeys) {
+      if (!sidsKeys.includes(roomName)) {
+        const participantSet = rooms.get(roomName);
+        const size = participantSet.size;
+
+        const participants = [];
+
+        for (let id of [...participantSet]) {
+          const userSocket = allSocket.get(id);
+          participants.push({
+            id: userSocket.id,
+            name: userSocket.name,
+          });
+        }
+        publicRooms.push({
+          id: "a" + roomId + Date.now(),
+          roomName,
+          size,
+          participants,
+        });
+        roomId++;
+      }
+    }
+    return publicRooms;
+  }
+
+  //set name event
+  socket.on("set_name", async (name, cb) => {
+    socket.name = name;
+    cb();
+    const activeUsers = await getActiveUsers();
+    io.emit("active_user", activeUsers);
+    const publicRoom = await getPublicRooms();
+    io.emit("get_public_room", publicRoom);
+  });
+
+  //disconnect event
+  socket.on("disconnect", async () => {
+    const activeUsers = await getActiveUsers();
+    io.emit("active_user", activeUsers);
+    const publicRoom = await getPublicRooms();
+    io.emit("get_public_room", publicRoom);
+  });
+
+  //send private message
+  socket.on("send_message", (data, cb) => {
+    const id = data.id;
+    const isRoom = data.isRoom === "false" ? false : data.isRoom;
+    data.isRoom = isRoom;
+    // receive-message
+    if (!data.message) return;
+    if (isRoom) {
+      socket.to(id).emit("receive_message", data, socket.id);
+      cb();
+    } else {
+      io.to(id).emit("receive_message", data, socket.id);
+      cb();
+    }
+  });
+
+  //create public room
+  socket.on("create_room", async (roomName, cb) => {
+    socket.join(roomName);
+    const publicRoom = await getPublicRooms();
+    io.emit("get_public_room", publicRoom);
+    cb();
+  });
+
+  //join room event
+  socket.on("join_room", async (roomName, cb) => {
+    socket.join(roomName);
+    const publicRoom = await getPublicRooms();
+    io.emit("get_public_room", publicRoom);
+    cb();
+  });
+
+  //leave room
+  socket.on("leave_room", async (roomName, cb) => {
+    socket.leave(roomName);
+    cb();
+    const publicRoom = await getPublicRooms();
+    io.emit("get_public_room", publicRoom);
+  });
+});
+
+//listening port 500
+expressServer.listen(5000, () => {
+  console.log("Server is running successfully");
+});
